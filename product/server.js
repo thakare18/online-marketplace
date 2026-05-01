17a3609eead32946d0d215af4e4b2af58d349100
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-05-01 23:25:02 +0530

    product service with Express, MongoDB, and necessary dependencies.

---
+++ product/server.js
@@ -0,0 +1,6 @@
+const app = require('./src/app');
+
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
---
+++ product/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+
+
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+
+
+module.exports = app;
+
