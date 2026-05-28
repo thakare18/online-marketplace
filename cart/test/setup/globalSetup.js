2da04af71d14b9a8cd6f4c9c25b14554223c0773
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-05-28 21:36:26 +0530

    remove cart service

--- cart/test/setup/globalSetup.js
+++
@@ -1,7 +0,0 @@
-// Global test setup for Cart service
-process.env.JWT_SECRET = process.env.JWT_SECRET || 'testsecret';
-
-// Optionally could set NODE_ENV
-afterAll(() => {
-    // cleanup hooks if needed later
-});
