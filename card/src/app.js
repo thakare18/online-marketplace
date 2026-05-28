fb50ec2270322acb8d5d2bec8e3d50f5bfc5428e
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-05-28 11:35:32 +0530

    card routes and authentication middleware; add item to card functionality

--- card/src/app.js
+++ card/src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
-
+const cardRoutes = require('./routes/card.routes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
+
+app.use('/api/cards', cardRoutes); //prefix for all card related routes
 
 module.exports = app;
---
+++ card/src/controller/card.controller.js
@@ -0,0 +1,10 @@
+const cardmodel = require('../model/card.model');
+
+async function addItemToCard(req, res) {
+
+}
+
+
+module.exports = {
+    addItemToCard
+}
