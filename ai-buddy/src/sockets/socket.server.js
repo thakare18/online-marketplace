621c29ca345649cca8fc5c236f29a3acc934b1cd
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-03 10:42:03 +0530

    JWT authentication middleware for socket connections

--- ai-buddy/src/sockets/socket.server.js
+++ ai-buddy/src/sockets/socket.server.js
@@ -1,9 +1,32 @@
 const { server } = require("socket.io");
+const jwt = require('jsonwebtoken');
+const cookie = require('cookie');
 
 
 async function initSocketServer(httpServer) {
 
     const io = new server(httpServer, {});
+
+
+    io.use((socket, next) => {
+        const cookies = cookie.parse(socket.handshake.headers.cookie);
+
+        const { token } = cookie ? cookie.parse(cookies) : {};
+
+        if (!token) {
+            return next(new Error('token Not provided'));
+        }
+
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            socket.user = decoded;
+            next();
+        } catch (error) {
+            return next(new Error('Invalid token'));
+        }
+
+    });
+
 
     io.on('connection', (socket) => {
         console.log('A user connected');
