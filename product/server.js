165ba7fe76f8edf80741df56ca0b12a56d9fc35e
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-14 21:27:07 +0530

    Added RabbitMQ integration with message publishing and consuming functionality

--- product/server.js
+++ product/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
+const { connect } = require('./src/broker/borker');
 
 
 async function startServer() {
     try {
         await connectDB();
+        await connect();
 
         app.listen(3001, () => {
             console.log('Server is running on port 3001');
--- product/src/broker/borker.js
+++
@@ -1,6 +0,0 @@
-async function publishToQueue(queueName, data) {
-    console.log(`[Mock Broker] Publishing to ${queueName}:`, data);
-    return true;
-}
-
-module.exports = { publishToQueue };
