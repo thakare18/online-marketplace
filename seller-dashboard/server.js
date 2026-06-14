4038838d5ed7101b105af14654986428712405a9
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-14 17:16:59 +0530

    Added RabbitMQ integration and MongoDB connection setup

--- seller-dashboard/server.js
+++ seller-dashboard/server.js
@@ -1,4 +1,10 @@
+require('dotenv').config();
 const app = require('./src/app');
+
+const connectDB = require('./src/db/db');
+
+
+connectDB();
 
 
 
---
+++ seller-dashboard/src/db/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to MongoDB");
+    }
+    catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
