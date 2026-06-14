23e291c0fe25f7ae0c287ed62b87f0155ae1ecad
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-14 20:58:38 +0530

    Added event listener for user creation in seller dashboard

--- seller-dashboard/src/broker/listener.js
+++ seller-dashboard/src/broker/listener.js
@@ -1,7 +1,13 @@
-const  subscribeToQueue  = require("./broker")
+const { subscribeToQueue } = require("./broker")
+const userModel = require("../models/user.model")
 
 
 
 module.exports = async function listenForEvents() {
+
+    await subscribeToQueue("AUTH_SELLER_DASHBOARD.USER_CREATED", async (user) => {
+        // Handle user created event
+        await userModel.create(user);
+    });
 
 }
