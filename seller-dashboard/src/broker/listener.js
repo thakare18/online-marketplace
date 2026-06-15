3f5fbf9f42bad0f3d7994bfe8fc3c140c4f47963
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-15 08:56:20 +0530

    add product creation event listener in seller dashboard

--- seller-dashboard/src/broker/listener.js
+++ seller-dashboard/src/broker/listener.js
@@ -1,5 +1,6 @@
 const { subscribeToQueue } = require("./broker")
 const userModel = require("../models/user.model")
+const productModel = require("../models/product.model")
 
 
 
@@ -8,6 +9,11 @@ module.exports = async function listenForEvents() {
     await subscribeToQueue("AUTH_SELLER_DASHBOARD.USER_CREATED", async (user) => {
         // Handle user created event
         await userModel.create(user);
+    });
+
+    await subscribeToQueue("AUTH_SELLER_DASHBOARD.PRODUCT_CREATED", async (product) => {
+        // Handle product created event, if product is created by seller then that replicate on seller-dashboard
+        await productModel.create(product);
     });
 
 }
