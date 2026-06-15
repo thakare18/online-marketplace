94cd049b97c29404daea938ad32649e25bc79e81
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-15 23:25:01 +0530

    added health check route for product service

--- product/src/app.js
+++ product/src/app.js
@@ -8,6 +8,13 @@ const productRoutes = require('./routes/product.routes');
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+
+//health check route
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Product service is running" });
+});
+
+
 app.use('/api/products', productRoutes);
 
 module.exports = app;
