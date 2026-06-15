70700b72969575667f5ae00eb1142931adb25517
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-15 22:55:15 +0530

    added health check route to verify server status

--- seller-dashboard/src/app.js
+++ seller-dashboard/src/app.js
@@ -7,6 +7,14 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+
+//health check route to check if server is running
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Seller Dashboard is running" });
+});
+
+// Routes
 app.use('/api/seller/dashboard', sellerRoutes); // prefix all seller dashboard routes with /api/seller/dashboard
 
 module.exports = app;
