edbfed34ab55a34cee50a1fbf07dc0bd2ed7e359
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-06-15 21:12:20 +0530

    restructure seller routes and integrate into main app

--- seller-dashboard/src/app.js
+++ seller-dashboard/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const sellerRoutes = require('./routes/seller.routes');
 
 const app = express();
 
 
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api/seller/dashboard', sellerRoutes);
 
 module.exports = app;
